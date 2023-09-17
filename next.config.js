076618fd7f19678889a603117675b6d8c991f571
Eric Wardner <ewardner22@gmail.com>
2023-09-17 15:18:02 -0400

    Remove export output from next config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-    basePath: '/hottest-place-next',
+    // output: 'export',
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    trailingSlash: true,
+    // trailingSlash: true,
    
     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     // skipTrailingSlashRedirect: true,
